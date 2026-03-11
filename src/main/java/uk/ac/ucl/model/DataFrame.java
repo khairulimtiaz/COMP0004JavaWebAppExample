42f59b30c18c020adcd47fd03c148850b47ea78a
khairulimtiaz <khairulimtiaz010@gmail.com>
2026-03-11 12:56:45 +0000

    create methods getValue, putValue, addValue, and private helper getColumn for the other methods

--- src/main/java/uk/ac/ucl/model/DataFrame.java
+++ src/main/java/uk/ac/ucl/model/DataFrame.java
@@ -11,6 +11,18 @@ public class DataFrame {
         columns.add(newColumn);
     }
 
+    private Column getColumn(String targetColumnName)
+    {
+        for (Column column: columns)
+        {
+            if (column.getName().equals(targetColumnName))
+            {
+                return column;
+            }
+        }
+        throw new  IllegalArgumentException("Column not found" + targetColumnName);
+    }
+
     public ArrayList<String> getColumnNames(){
         ArrayList<String> names = new ArrayList<>();
         for (Column column:columns)
@@ -27,37 +39,15 @@ public class DataFrame {
         return columns.get(0).getSize();
     }
 
-    public String getValue(String columnName, int row)
-    {
-        for (Column column : columns)
-        {
-            if (column.getName().equals(columnName))
-            {
-                return column.getRowValue(row);
-            }
-        }
-        throw new  IllegalArgumentException("Column not found" + columnName);
+    public String getValue(String columnName, int row) {
+        return getColumn(columnName).getRowValue(row);
     }
 
-    public void putValue(String columnName, int row, String value){
-        for (Column column:columns)
-        {
-            if (column.getName().equals(columnName)){
-                column.setRowValue(row,value);
-                break;
-            }
-        }
-        throw new  IllegalArgumentException("Column not found" + columnName);
+    public void putValue(String columnName, int row, String value) {
+         getColumn(columnName).setRowValue(row,value);
     }
 
     public void addValue(String columnName, String value){
-        for (Column column:columns)
-        {
-            if (column.getName().equals(columnName)){
-                column.addRowValue(value);
-                break;
-            }
-            throw new  IllegalArgumentException("Column not found" + columnName);
-        }
+        getColumn(columnName).addRowValue(value);
     }
 }
